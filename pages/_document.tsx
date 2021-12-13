3f6b172b0ed1f1ffc0e24edec428a16a62cea012
luqaska <83518257+luqaska@users.noreply.github.com>
2021-12-13 19:11:46 -0300

    now it should work...

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,1 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
 
-import { GA_TRACKING_ID } from "../functions/gtag";
-
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <!-- https://youtu.be/O2C5R3FOWdE -->
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
