93a2b0cdb2d937c32990eafae2e61eb2206ab62d
luqaska <83518257+luqaska@users.noreply.github.com>
2021-12-13 19:09:36 -0300

    fuck google analytics, long live to our dear lord and saviour, garfeldi

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,23 +7,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-          `
-            }}
-          />
+          <!-- https://youtu.be/O2C5R3FOWdE -->
         </Head>
         <body>
           <Main />
