7e56990cdd027a1236a94b3bfbaca033b6cbffd0
luqaska <83518257+luqaska@users.noreply.github.com>
2021-12-13 20:44:22 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,26 @@
-import Document, { Html, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+import { GA_TRACKING_ID } from "../functions/gtag";
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
