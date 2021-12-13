6f65efcaeb9eb0c38dcd8f08e477a3d299230c59
luqaska <83518257+luqaska@users.noreply.github.com>
2021-12-13 19:12:54 -0300

    github

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,1 +1,16 @@
-
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../functions/gtag";
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
